eda20e2a4f4d30452db4b74fb4933404b5b54c83
HuzaifaInshal <huzaifainshal@gmail.com>
2023-06-05 01:45:06 +0500

    added frontend

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+import image1 from '../background images/1.jpeg';
+import image2 from '../background images/2.jpeg';
+import image3 from '../background images/3.jpeg';
+import image4 from '../background images/4.jpeg';
+import image5 from '../background images/5.jpeg';
+import image6 from '../background images/6.jpeg';
+import Start from './Start';
+import About from './About';
+import Info from './Info';
+
+const Main = () => {
+  const [backgroundImage, setBackgroundImage] = useState(image1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [count, setCount] = useState(5);
+
+  const imageUrls = [
+    image2,image3,image4,image5,image6
+  ];
+        const [activeSection, setActiveSection] = useState('');
+      
+        useEffect(() => {
+          const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const sections = document.querySelectorAll('section');
+      
+            sections.forEach((section) => {
+              const sectionTop = section.offsetTop;
+              const sectionHeight = section.offsetHeight;
+      
+              if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+                setActiveSection(section.id);
+              }
+            });
+          };
+          window.addEventListener('scroll', handleScroll);
+          return () => {
+            window.removeEventListener('scroll', handleScroll);
+          };
+        }, []);
+
+        useEffect(() => {
+          const timer = setInterval(() => {
+            setCount((prevCount) => {
+              if (prevCount === 0) {
+                return 5;
+              } else {
+                return prevCount - 1;
+              }
+            });
+          }, 1000);
+      
+          return () => {
+            clearInterval(timer);
+          };
+        }, []);
+
+        // useEffect(()=>{
+        //   if(count===0){
+        //     setBackgroundImage(imageUrls[currentIndex])
+        //     setCurrentIndex(currentIndex + 1)
+        //     if(currentIndex===4){
+        //       setCurrentIndex(0)
+        //     }
+        //   }
+        //   // eslint-disable-next-line
+        // },[count])
+        
+
+  return (
+      <div className='container' style={{ backgroundImage: `url(${backgroundImage})` }}>
+      
+    <nav>
+        <div className="logoholder">
+            <div className="logoholder2">
+            <img src="./external assets/logo2.png" alt="" className="logo" />
+            </div>
+        </div>
+      <ul>
+        <li className={activeSection === 'start' ? 'active' : ''} style={activeSection === 'start' ? { '--colora': 'red' } : { '--colora': 'yellow' }}>
+          <a href="#start">Start</a>
+        </li>
+        <li className={activeSection === 'about' ? 'active' : ''} style={activeSection === 'about' ? { '--colora': 'blue' } : { '--colora': 'yellow' }}>
+          <a href="#about">About</a>
+        </li>
+        <li className={activeSection === 'info' ? 'active' : ''} style={activeSection === 'info' ? { '--colora': 'yello' } : { '--colora': 'yellow' }}>
+          <a href="#info">Info</a>
+        </li>
+        <li>
+            <button className='btn-begin'>Lets Begin</button>
+        </li>
+      </ul>
+    </nav>
+    <div className="holder">
+    <div className="holder2">
+    <section id='start'><Start/></section>
+    <section id='about'><About/></section>
+    <section id='info'><Info/></section>
+    </div>
+    </div>
+    <p className='counter'>Timer: {count}</p>
+    </div>
+  )
+}
+
+export default Main
---
+++ frontend/src/components/Start.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Start = () => {
+  return (
+    <>
+    <h1 className="youmusic">YOUMUSIC</h1>
+    </>
+  )
+}
+
+export default Start
