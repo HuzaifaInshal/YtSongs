f684907788acfce03d618837149b423bc6441cdb
HuzaifaInshal <huzaifainshal@gmail.com>
2023-06-15 23:43:57 +0500

    final-commit

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.use('/search',require('./backend/routes/searchRoutes'))
-app.use('/download',require('./backend/routes/download'))
+app.use('/search',require('./routes/searchRoutes'))
+app.use('/download',require('./routes/download'))
 
-app.use(express.static(path.join(__dirname,'../ytmusic/frontend/build/')))
+app.use(express.static(path.join(__dirname,'../frontend/build/')))
 app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
 
 
