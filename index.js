fa72c28617a31488ca8f70869b96f112832d7144
HuzaifaInshal <huzaifainshal@gmail.com>
2023-08-14 03:52:06 +0500

    modifications for vercel

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/search',require('./backend/routes/searchRoutes'))
+app.use('/download',require('./backend/routes/download'))
+
+app.use(express.static(path.join(__dirname,'./frontend/build/')))
+app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'./','frontend','build','index.html')))
+
+
+app.listen(4000,()=>console.log(`Server is listening at port 4000`))
+
